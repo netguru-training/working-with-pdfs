56b29ed8aeb3bd6565c7784913811d777e217df2
Jakub Kubacki <kubacki.jk@gmail.com>
2016-05-06 16:21:03 +0200

    Add route for wicked pdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'prawn' => 'prawn#generate'
+  get 'wicked_pdf' => 'wicked_pdf#generate'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
